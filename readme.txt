7aafa8336763b3355213bec444a79b259e565670
Joseph Neathawk <11905983+knighthawk0811@users.noreply.github.com>
2021-07-14 12:20:01 -0500

    FPL

---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== VI: Components Bootstrap 4 ===
+Contributors: Knighthawk
+Donate link:
+Tags:
+Requires at least: ?
+Tested up to: 5.3
+Version: 9.2.200903
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Site Specific Functions
+
+== Description ==
+
+Just include the Bootstrap Components, on the public facing side of the website, nothing more
+
+
+== Changelog ==
+
+= 9.2.200903 =
+* updated to bootstrap version 4.5.2
+
+= 9.1.200310 =
+* added priority of 5
+Effor to to be bumped earlier so that the theme style will have a chance to override naturally.
+
+= 9.1.191204 =
+* FPL v9
---
+++ vi_components_bootstrap.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: VI: Components Bootstrap 5
+Plugin URI: http://neathawk.com
+Description: Just include the Bootstrap Components, on the public facing side of the website, nothing more. NOTE: Bootstrap requires jQuery.
+Version: 9.1.210714
+Author: Joseph Neathawk
+Author URI: http://Neathawk.com
+License: GNU General Public License v2 or later
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+class vi_components_bootstrap_5
+{
+    /*--------------------------------------------------------------
+    >>> TABLE OF CONTENTS:
+    ----------------------------------------------------------------
+    # Instructions
+    # TODO
+    # Attributes
+    # Constructive Functions
+    # Reusable Functions
+    # Shortcode Functions (are plugin territory)
+    --------------------------------------------------------------*/
+
+
+    /*--------------------------------------------------------------
+    # TODO
+    --------------------------------------------------------------*/
+
+    //
+
+    /*--------------------------------------------------------------
+    # Attributes
+    --------------------------------------------------------------*/
+    private static $error_report = false;
+
+    /*--------------------------------------------------------------
+    # Constructive Functions
+    --------------------------------------------------------------*/
+	/**
+	 * ENQUEUE SCRIPTS AND STYLES
+	 *
+	 * wp_enqueue_style( string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, string $media = 'all' )
+	 * wp_enqueue_script( string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, bool $in_footer = false )
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/including-css-javascript/#stylesheets
+	 * @link https://getbootstrap.com/docs/5.0/getting-started/introduction/
+	 * @link https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+	 * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+	 * @version 9.3.210302
+	 * @since 9.1.191204
+	 */
+	public static function enqueue_scripts() {
+	    //style for the plugin
+	    wp_enqueue_style( 'vi-bc-css', plugins_url( '/style.css', __FILE__ ), NULL , NULL , 'all' );
+        //javascript for the plugin
+	    //wp_enqueue_script( 'vi-bc-js', plugins_url( '/common.js', __FILE__ ), array('jquery') , NULL , true );
+
+	    //bootstrap 5 components
+	    wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', array(), '5.0.2' );
+
+	    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.0.2', true );
+	}
+
+
+    /*--------------------------------------------------------------
+    # Reusable Functions
+    --------------------------------------------------------------*/
+
+
+
+    /*--------------------------------------------------------------
+    # Shortcode Functions (are plugin territory)
+    --------------------------------------------------------------*/
+
+
+}
+
+
+
+//enqueue scripts
+add_action( 'wp_enqueue_scripts', Array( 'vi_components_bootstrap_5', 'enqueue_scripts' ) );
+
